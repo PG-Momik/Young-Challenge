39efd4cb12e4c0d7f76f0cd4fb22882b47dc8400
PG-Momik <momik12339@gmail.com>
2022-08-16 22:42:54 +0545

    Endpoints defined

---
+++ API/index.php
@@ -0,0 +1,55 @@
+<?php
+
+/*format : baseurl/entity/operation/optional
+ *example: localhost/Challenge/API/getAll
+ *      : localhost/Challenge/API/getAll/2
+ *     : localhost/Challenge/API/average/2010-2017
+*/
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Method: POST, READ, PUT, DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+
+$url  =  $_SERVER['REQUEST_URI'];
+$keys = explode('/', $url);
+$entity = $keys[2];
+$operation = $keys[3];
+
+require_once 'Models/Sales.php';
+
+switch ($_SERVER['REQUEST_METHOD']){
+    case "GET":
+        switch ($entity){
+            case "sales":
+                $salesTable = new Sales('../DB');
+                switch ($operation){
+                    case "all":
+
+                        break;
+                    case "products":
+
+                        break;
+                    case "winners":
+
+                        break;
+                    case "losers":
+
+                        break;
+                    case "average":
+
+                        break;
+                    default:
+                        echo json_encode(array("400 Invalid Operation."));
+                }
+                break;
+            default:
+                echo json_encode(array("Under construction"));
+                break;
+        }
+        break;
+    default:
+        echo json_encode(array("Illegal Method."));
+        break;
+}
