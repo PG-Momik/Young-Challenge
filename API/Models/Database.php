549c8874a424c42a0e2c0fa5f5437ebc39ed312f
PG-Momik <momik12339@gmail.com>
2022-08-16 22:54:47 +0545

    CreateTable() works

--- API/Models/Database.php
+++ API/Models/Database.php
@@ -8,7 +8,7 @@ class Database
 
     public function __construct($dbPath)
     {
-        $this->DB_NAME =$dbPath.$this->DB_NAME;
+   		$this->DB_NAME = $dbPath.$this->DB_NAME;
         $this->connect();
     }
 
@@ -17,7 +17,7 @@ class Database
         try {
             $this->conn = new PDO("sqlite:" . $this->DB_NAME);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Vayo";
+            return $this->conn;
         } catch (PDOException $e) {
             echo "Connection error:" . $e->getMessage();
             return false;
@@ -25,5 +25,29 @@ class Database
     }
 
 
-   
+    //Almost Common methods
+    function createTable(): bool
+    {
+        $queries = [
+            "CREATE TABLE IF NOT EXISTS Countries (ID INTEGER NOT NULL, Name Char(50) UNIQUE, PRIMARY KEY(ID))",
+            "CREATE TABLE IF NOT EXISTS Petroleum_Product (ID INTEGER NOT NULL, Name Char(50) UNIQUE, PRIMARY KEY(ID))",
+            "CREATE TABLE IF NOT EXISTS Sales (ID INTEGER NOT NULL, Year INTEGER, Petroleum_Id INTEGER, Sale INTEGER, Country_Id INTEGER, FOREIGN KEY(Country_Id) REFERENCES Countries(ID), FOREIGN KEY(Petroleum_Id) REFERENCES Petroleum_Product(ID), PRIMARY KEY(ID))"
+        ];
+        switch ($this->table) {
+            case "Countries":
+                $stmt = $this->conn->prepare($queries[0]);
+                return $stmt->execute() ?? false;
+            case "Petroleum_Product":
+                $stmt = $this->conn->prepare($queries[1]);
+                return $stmt->execute() ?? false;
+            case "Sales":
+                $stmt = $this->conn->prepare($queries[2]);
+                return $stmt->execute() ?? false;
+            default:
+                return false;
+        }
+    }
+
+ 
+
 }
