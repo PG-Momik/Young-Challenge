8eeda938076354c6358204901a87b42bb17fffb6
PG-Momik <momik12339@gmail.com>
2022-08-16 22:48:36 +0545

    Sanity chek pass:works

--- API/Models/Database.php
+++ API/Models/Database.php
@@ -3,6 +3,7 @@
 
 class Database
 {
+    private string $DB_NAME = "../DB/PetroliumDB.db";
     protected PDO $conn;
 
     public function __construct()
@@ -10,5 +11,18 @@ class Database
         $this->connect();
     }
 
- 
+    public function connect(): bool|PDO
+    {
+        try {
+            $this->conn = new PDO("sqlite:" . $this->DB_NAME);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Vayo";
+        } catch (PDOException $e) {
+            echo "Connection error:" . $e->getMessage();
+            return false;
+        }
+    }
+
+
+   
 }
