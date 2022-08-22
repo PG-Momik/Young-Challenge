417939335d40c6b6649a228ba99e9529f9def221
PG-Momik <momik12339@gmail.com>
2022-08-22 09:24:36 +0545

    Fixed url issue

--- API/index.php
+++ API/index.php
@@ -1,9 +1,9 @@
 <?php
 
 /*format : baseurl/entity/operation/optional
- *example: localhost/Challenge/API/getAll
- *      : localhost/Challenge/API/getAll/2
- *     : localhost/Challenge/API/average/2010-2017
+ *example: localhost/API/getAll
+ *      : localhost/API/getAll/2
+ *     : localhost/API/average/2010-2017
 */
 
 header('Access-Control-Allow-Origin: *');
@@ -11,11 +11,18 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Method: POST, READ, PUT, DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-
 $url  =  $_SERVER['REQUEST_URI'];
 $keys = explode('/', $url);
-$entity = $keys[3];
-$operation = $keys[4];
+
+if(in_array("Challenge", $keys)){
+    $entity = $keys[3]??"wrong";
+    $operation = $keys[4]??"wrong";
+    $options= $keys[5]??null;    
+}else{
+    $entity =$keys[2]??"wrong";
+    $operation = $keys[3]??"wrong";
+    $options= $keys[4]??null;
+}
 
 require_once 'Models/Sales.php';
 
@@ -27,8 +34,8 @@ switch ($_SERVER['REQUEST_METHOD']){
                 switch ($operation){
                     case "all":
                         $page = 1;
-                        if(isset($keys[5])){
-                            $page = $keys[5];
+                        if(!is_null($options)){
+                            $page = $options;
                         }
                         $result  = $salesTable->getAll(page:$page);
                         echo json_encode($result);
@@ -39,26 +46,26 @@ switch ($_SERVER['REQUEST_METHOD']){
                         break;
                     case "winners":
                         $num = 3;
-                        if(isset($keys[5])){
-                            $num = $keys[5];
+                        if(!is_null($options)){
+                            $num = $options;
                         }
                         $result  = $salesTable->getWinners($num);
                         echo json_encode($result);
                         break;
                     case "losers":
                         $num = 3;
-                        if(isset($keys[5])){
-                            $num = $keys[5];
+                        if(!is_null($options)){
+                            $num = $options;
                         }
                         $result  = $salesTable->getLosers($num);
                         echo json_encode($result);
                         break;
                     case "average":
-                        if(!isset($keys[5])){
+                        if(is_null($options)){
                             echo json_encode(array("Invalid URL format."));
                             break;
                         }
-                        $years = explode("-", $keys[5]);
+                        $years = explode("-", $options);
                         $result  = $salesTable->getAverage($years[0], $years[1]);
                         echo json_encode($result);
 
